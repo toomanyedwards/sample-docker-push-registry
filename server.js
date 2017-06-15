6e0cd9850eaa61e382452097b75b932bb9a26311
toomanyedwards <toomanyedwards@gmail.com>
2017-06-14 22:28:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var app = express(); 
 
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, '/static')));
